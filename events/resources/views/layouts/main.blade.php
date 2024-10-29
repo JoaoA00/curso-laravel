7dda342d4001eafe6f3b6a3303a0a32a7a30c84e
João Abner <joaoabner2022@gmail.com>
2024-10-29 19:28:55 -0300

    Confirmar criação do evento
    Exibir uma mensagem na tela inicial indicando o sucesso na criação do envendo

--- events/resources/views/layouts/main.blade.php
+++ events/resources/views/layouts/main.blade.php
@@ -44,8 +44,18 @@
                 </div>
             </nav>
         </header>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
+        </main>
         
-        @yield('content')
+
 
         <footer>
             <p>Events &copy; 2024</p>
