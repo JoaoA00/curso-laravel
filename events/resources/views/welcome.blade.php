4d9bc4cb3e4f7dc89cee7192c1bb909587ab3b9a
João Abner <joaoabner2022@gmail.com>
2024-10-29 10:15:39 -0300

    Finalizar a Home
    Estilizando e adicionando a barra de pesquisa e os cards de eventos da home

--- events/resources/views/welcome.blade.php
+++ events/resources/views/welcome.blade.php
@@ -4,11 +4,28 @@
 
 @section('content')
 
-@foreach($events as $event)
-    <p>{{ $event->title }}</p>
-    <p>{{ $event->description }}</p>
-    <p>{{ $event->city }}</p>
-    <p>{{ $event->private }}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Busque o evento</h1>
+    <form action="">
+        <input type="text" id="search" class="form-control" placeholder="Procurar">
+    </form>
+</div>
+<div id="events-container" class="cold-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/auditório-cheio.jpg" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <p class="card-date">10/08/2025</p>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants">2 participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
