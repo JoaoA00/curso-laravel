1b2cf18e5818309cf365acb6eaef166f61ce0945
João Abner <joaoabner2022@gmail.com>
2024-11-04 10:03:33 -0300

    Adicionar imagem de capa do evento

--- events/resources/views/welcome.blade.php
+++ events/resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
             <div class="card col-md-3">
-                <img src="/img/auditório-cheio.jpg" alt="{{ $event->title }}">
+                <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                 <div class="card-body">
                     <p class="card-date">10/08/2025</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
