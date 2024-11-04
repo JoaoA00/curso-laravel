dd89a9bd9cfd1940dd43555eedbc67d4dc73952c
João Abner <joaoabner2022@gmail.com>
2024-11-04 11:56:40 -0300

    Corrigindo erro da migration 'items'

--- events/database/migrations/2024_11_04_140221_add_items_to_events_table.php
+++ events/database/migrations/2024_11_04_140221_add_items_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->json('itens');
+            $table->json('items');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('itens');
+            $table->dropColumn('items');
         });
     }
 };
